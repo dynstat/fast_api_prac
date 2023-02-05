e768d5e5f520e1f9dc1b8d453959fc6ac5f42952
dynstat <vs15vivek@gmail.com>
2023-02-05 22:02:19 +0530

    testing path n query params

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 class Item(BaseModel):
@@ -25,3 +32,19 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
