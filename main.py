88d18ce0c2444bed277922957198409dd36b8127
dynstat <vs15vivek@gmail.com>
2023-02-12 10:13:12 +0530

    customized default 422 error according to different parameters

--- main.py
+++ main.py
@@ -35,14 +35,29 @@ app = FastAPI(exception_handlers=exc_handlers)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
-    # if request.path_params
+    exc_list = exc.errors()
+    resp_json_list = []
+    for e in exc_list:
+        temp_dict = {}
+        std_err_mssg = e["msg"]
+        type_ = e["loc"][0]
+        name = e["loc"][1]
+        add_info = f"422 ERROR - Please check the type of {type_} named {name}."
+        # filling the temp dictionary
+        temp_dict[type_] = name
+        temp_dict["std error"] = std_err_mssg
+        temp_dict["additional info"] = add_info
+
+        # appending this temp dict to resp_json_list
+        resp_json_list.append(temp_dict)
+
+    path_params = request.path_params
+    query_params = request.query_params._dict
     # return PlainTextResponse(str(exc), status_code=422)
-    return JSONResponse({"detail": "ye to galat h re"}, status_code=422)
-
-
-# @app.exception_handler(422)
-# async def error422(request: Request, exc: HTTPException):
-#     return {"error422": "ye to bekar error hai"}
+    return JSONResponse(
+        resp_json_list,
+        status_code=422,
+    )
 
 
 class Message(BaseModel):
@@ -89,9 +104,11 @@ async def read_items(
     return results
 
 
-@app.get("/test/{p1}")
-async def read_items(p1: int):
-    resp = {"p1": p1}
+@app.get("/test/{p1}/{p2}")
+async def read_items(p1: int, p2: int, q: int | None = None):
+    resp = {"p1": p1, "p2": p2}
+    if q:
+        resp["q"] = q
     return resp
 
 
