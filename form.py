79524b09a35a14ec639c6d9dfca854ce9794cbad
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-02-14 09:25:30 +0530

    all files are removed and moved inside testing folder.

--- form.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI,Form
-
-app = FastAPI()
-
-@app.post("/login/")
-
-async def form_prac(item_id:int,username:str=Form(),password:str = Form()):
-    return {"username":username}
--- testApi.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-my_header = {"x-token": ""}
-
-
-resp = requests.get("http://127.0.0.1:8000/hitems", headers=my_header)
-
-print(resp.content)
