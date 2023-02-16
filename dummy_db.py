ae19c89093cd346d134780a5a916d994233f5500
dynstat <vs15vivek@gmail.com>
2023-02-17 03:49:23 +0530

    password value changed to lower case (not imp)

--- dummy_db.py
+++ dummy_db.py
@@ -3,6 +3,6 @@ dummy_DB = [
         "username": "xs",
         "full_name": "Vivek",
         "email": "xs@gmail.com",
-        "plain_pass": "somePass",
+        "plain_pass": "somepass",
     }
 ]
