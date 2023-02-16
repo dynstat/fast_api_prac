8a38f8c3e804d2baaf467b6c0a398bba529f1730
dynstat <vs15vivek@gmail.com>
2023-02-16 19:57:05 +0530

    testing cookie

--- testApi.py
+++ testApi.py
@@ -1,8 +1,8 @@
 import requests
 
 my_header = {"x-token": ""}
+my_cookie = {"meri_cookie2": "hohohoho"}
 
-
-resp = requests.get("http://127.0.0.1:8000/hitems", headers=my_header)
+resp = requests.get("http://127.0.0.1:8000/test3", headers=my_header, cookies=my_cookie)
 
 print(resp.content)
