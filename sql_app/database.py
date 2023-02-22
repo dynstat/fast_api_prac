152d38670d1bde92f8fca3c35ef05c6828748db1
dynstat <vs15vivek@gmail.com>
2023-02-23 02:02:01 +0530

    database.py created and linked withing the sql_app/main.py with migrations stuffs

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "sqlite:///./sql_app.db"
+DB_URL = "sqlite:///./sql_app_DB.sqlite3."
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 
