99ffc8e0f961dc9190e623212b4ab07f0278e783
dynstat <vs15vivek@gmail.com>
2023-02-24 00:40:58 +0530

    comments added

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,8 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 DB_URL = "sqlite:///./sql_app/sql_app_DB.sqlite3."
 
+# Creating an engine to create and connect to the database provided.
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 
+# dB session can be created using sessionLocal class
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# to be used in model classes (for databases)
 DeclBase = declarative_base()
