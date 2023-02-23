eaa213bf752dde03327cdadcb29d4b0a21dfdbcc
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-02-23 20:42:48 +0530

    Revert "schemas.py modified"
    This reverts commit 8bedadd60b39c02c73bc006b906d7d20e70107bf.

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,22 +1,12 @@
 from pydantic import BaseModel
 
 
-# class ItemBase(BaseModel):
-#     title: str
-#     description: str | None = None
+class User(BaseModel):
+    id: int
+    email: str
 
-
-# class ItemCreate(ItemBase):
-#     pass
-
-
-# class Item(ItemBase):
-#     id: int
-#     owner_id: int
-
-#     class Config:
-#         orm_mode = True
-
+    class Config:
+        orm_mode = True
 
 class UserBase(BaseModel):
     email: str
@@ -29,7 +19,7 @@ class UserCreate(UserBase):
 class User(UserBase):
     id: int
     is_active: bool
-    # items: list[Item] = []
+    items: list[Item] = []
 
     class Config:
         orm_mode = True
