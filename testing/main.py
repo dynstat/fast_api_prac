751adfe4aab3d1f7963dede34aec4a08de7ad537
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-02-24 10:40:45 +0530

    some files added in testing folder

--- testing/main.py
+++ testing/main.py
@@ -1,41 +1,75 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
-
-app = FastAPI()
-
-
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: str | None = None
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker,Session
+from sqlalchemy import Boolean, Column, Integer, String
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
+SQLALCHEMY_DATABASE_URL = ("sqlite:///test.sqlite3")
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+# The first step is to create a SQLAlchemy "engine".
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},echo = True)
+'''
+Each instance of the SessionLocal class will be a database session. The class itself is not a database session yet.
+
+But once we create an instance of the SessionLocal class, this instance will be the actual database session.
+
+We name it SessionLocal to distinguish it from the Session we are importing from SQLAlchemy.
+'''
+
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+Base = declarative_base()
 
 
-class UserInDB(BaseModel):
-    username: str
-    hashed_password: str
-    email: EmailStr
-    full_name: str | None = None
 
 
-def fake_password_hasher(raw_password: str):
-    return "supersecret" + raw_password
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    
+    
+#to migrate
+
+Base.metadata.create_all(engine) 
+
+# create data into table 
+u1 =User(id=1,email="k@k.com",hashed_password="123abc@",is_active=True)
+u2 =User(id=2,email="v@v.com",hashed_password="124abc@",is_active=True)
+u3 =User(id=3,email="k@v.com",hashed_password="124abc@",is_active=True)
+u4 =User(id=4,email="b@v.com",hashed_password="124abc@",is_active=True)
+u5 =User(id=5,email="i@v.com",hashed_password="124abc@",is_active=True)
+# session.add(u1)
+session.add_all([u1,u2,u3,u4,u5])
+session.commit()
 
 
-def fake_save_user(user_in: UserIn):
-    hashed_password = fake_password_hasher(user_in.password)
-    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
-    print("User saved! ..not really")
-    return user_in_db
+# '''get all data'''
+# user = session.query(User)
+
+# for u in user:
+#     print(u.email)
 
 
-@app.post("/user/", response_model=UserOut)
-async def create_user(user_in: UserIn):
-    user_saved = fake_save_user(user_in)
-    return user_saved
+# '''get data in order'''
+# user = session.query(User).order_by(User.email)
+
+# for u in user:
+#     print(u.email)
+
+# '''get data by filtering'''
+# user = session.query(User).filter(User.hashed_password=="123abc@").first()
+# print(user.email)
+# for u in user:
+#     print(u.email)
+
+# '''update data'''
+# user=session.query(User).filter(User.email=="k@k.com").first()
+# print(user.email)
+# user.email = "kv@kv.com"
+# session.commit()
