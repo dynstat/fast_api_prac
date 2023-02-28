e1ab5015290de6ef90db8db0f578299fc14a9d3e
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-02-28 10:40:12 +0530

    main.py file modified

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI, Request, Depends, Form, status
 from fastapi.templating import Jinja2Templates
 from . import models
-from database import sessionLocal, engine
+from .database import sessionlocal, engine
 from sqlalchemy.orm import Session
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
-models.Base.metadata.createall(bind=engine)
+models.base.metadata.create_all(bind=engine)
 
-templates = Jinja2Templates(directory='templates') 
+templates = Jinja2Templates(directory='todo_app/templates') 
 
 app = FastAPI()
 
 '''"Mounting" means adding a complete "independent" application in a specific path, that then takes care of handling all the sub-paths.'''
-app.mount("/static", StaticFiles(directory="static"),name="static")
+app.mount("/static", StaticFiles(directory="todo_app/static/css"),name="static")
 
 
 def get_db():
-    db = sessionLocal()
+    db = sessionlocal()
     try:
         yield db 
     finally:
@@ -26,36 +26,36 @@ def get_db():
 
 # to get items(tasks) from db 
 @app.get("/")
-async def get_task_list(req:Request, db:Session = Depends(get_db)):
+async def index(request:Request, db:Session = Depends(get_db)):
     to_dos = db.query(models.Todo).order_by(models.Todo.id.desc()) 
-    return templates.TemplateResponse("index.html",{"req":req,"to_dos":to_dos})
+    return templates.TemplateResponse("index.html",{"request":request,"to_dos":to_dos})
     
     
 @app.post("/add_task")
-async def create_tasks(req:Request, task:str=Form(), db:Session=Depends(get_db)):
+async def create_tasks(request:Request, task:str=Form(...), db:Session=Depends(get_db)):
     task_todo = models.Todo(task=task)  
     db.add(task_todo)
     db.commit()   
-    return RedirectResponse(url=app.url_path_for("home"),status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url=app.url_path_for("index"),status_code=status.HTTP_303_SEE_OTHER)
 
 
 @app.get("/edit/{todo_id}")
-async def add(req:Request, todo_id:int, db:Session=Depends(get_db)):
+async def add(request:Request, todo_id:int, db:Session=Depends(get_db)):
     todo = db.query(models.Todo).filter(models.Todo.id==todo_id).first()
-    return templates.TemplateResponse("edit.html",{"req":req,"todo":todo})
+    return templates.TemplateResponse("edit.html",{"request":request,"todo":todo})
     
 
 @app.post("/edit/{todo_id}")
-async def edit_task(req:Request,todo_id:int,db:Session=Depends(get_db),task:str= Form(),completed:bool=Form(False)):
+async def edit_task(request:Request,todo_id:int,db:Session=Depends(get_db),task:str= Form(),completed:bool=Form(False)):
     todo= db.query(models.Todo).filter(models.Todo.id==todo_id).first()
     todo.task = task
     todo.completed = completed
     db.commit()
-    return RedirectResponse(url = app.url_path_for("home"),status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url = app.url_path_for("index"),status_code=status.HTTP_303_SEE_OTHER)
 
 @app.get("/delete/{todo_id}")
-async def delete_task(req:Request,todo_id:int, db:Session = Depends(get_db)):
+async def delete_task(request:Request,todo_id:int, db:Session = Depends(get_db)):
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     db.delete(todo)
     db.commit()
-    return RedirectResponse(url=app.url_path_for("home"),status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url=app.url_path_for("index"),status_code=status.HTTP_303_SEE_OTHER)
