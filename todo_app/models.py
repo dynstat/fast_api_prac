fe644cd7744f4d52c905f8e565ed1f043be26323
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-02-28 10:40:46 +0530

    models.py file is added

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, Integer, Text, Boolean
-from database import base
+from .database import base
 
 class Todo(base):
-    __tablename__ = "ToDo's"
+    __tablename__ = "ToDo"
     id = Column(Integer,primary_key = True)
     task = Column(Text)
     completed = Column(Boolean)
