d8b1cac9494beeda3b586f35cd55f2872a63a281
dynstat <vs15vivek@gmail.com>
2023-03-09 21:17:19 +0530

    comments added in dependencies function. Related to the name of the parameters for the headers as we can not write hyphen i.e. "-" in python variable names, Therefore underscores are used.

--- app/dependencies_.py
+++ app/dependencies_.py
@@ -1,11 +1,14 @@
 from fastapi import HTTPException, Header
 
 
+# NOTE: The name of the parameter is x_token and x_key for the headers X-Token and X-Key respectively, since we can not put "-" in the python parameter variables.
+# async function to verify the value of the header field named X-token is "token1" or not.
 async def verify_token(x_token: str = Header(default="token1")):
     if x_token != "token1":
         raise HTTPException(status_code=400, detail="X-Token header is not token1")
 
 
+# async function to verify the value of the header field named X-key is "token1" or not.
 async def verify_key(x_key: str = Header(default="key1")):
     if x_key != "key1":
         raise HTTPException(status_code=400, detail="X-Key header is not key1")
