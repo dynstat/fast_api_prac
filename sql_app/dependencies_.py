e4cb5b896892fd6180715c9f23914fa38388d758
dynstat <vs15vivek@gmail.com>
2023-03-12 23:30:39 +0530

    comments added for get_db function

--- sql_app/dependencies_.py
+++ sql_app/dependencies_.py
@@ -2,10 +2,10 @@ from sqlalchemy.orm import Session
 from .database import sessionLocal
 
 
-# dB dependency
+# dB dependency. This function creates a generator, the yield statement returns a value and haults..without exiting the function.
 def get_db():
     db: Session = sessionLocal()
     try:
-        yield db
+        yield db  # sends the created database session
     finally:
-        db.close()
+        db.close()  # this statemnt gets excuted only in the end of function body.
