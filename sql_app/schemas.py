17022af9a0701eaf36e82656d21adebd29cb2da8
dynstat <vs15vivek@gmail.com>
2023-03-13 14:16:20 +0530

    added comments to the schemas

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 
 
+# Creating this class similar to SQLAlchemy's declarative_base() object, mainly for validation purposes
 class User(BaseModel):
     id: int
     email: str
 
+    # By defining the Config subclass, and setting the attribute "orm_mode" to True helps the response_model parameter to validate the SqlAlchemy's return User object even if it is not in the form of dictionary.
     class Config:
         orm_mode = True
 
