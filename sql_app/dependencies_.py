bad56a14febb61afdcbb33837cd8d3531d7a09d3
dynstat <vs15vivek@gmail.com>
2023-03-14 21:24:12 +0530

    comments added to the dependencies_.py

--- sql_app/dependencies_.py
+++ sql_app/dependencies_.py
@@ -6,6 +6,6 @@ from .database import sessionLocal
 def get_db():
     db: Session = sessionLocal()
     try:
-        yield db  # sends the created database session
+        yield db  # sends the created database session and haults
     finally:
-        db.close()  # this statemnt gets excuted only in the end of function body.
+        db.close()  # this statement gets excuted only in the end of function body.
