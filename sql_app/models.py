8aa332e9f7177fb0b20ae2b079ce15ccb456fe86
dynstat <vs15vivek@gmail.com>
2023-03-14 21:24:29 +0530

    explanatory comments added

--- sql_app/models.py
+++ sql_app/models.py
@@ -5,14 +5,17 @@ from sqlalchemy.orm import relationship
 from .database import DeclBase
 
 
+#  These model classes are used for creating the tables in the database.
 class User(DeclBase):
-    __tablename__ = "users"
+    __tablename__ = "users"  # defines the name of table in the database
 
+    # column names and their value types
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
+    # indicates that the column items will be related to the Item table values. It will consist of all the primary key values of Item table as a list.
     items = relationship("Item", back_populates="owner")
 
 
